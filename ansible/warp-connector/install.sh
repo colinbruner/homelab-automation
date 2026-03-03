1812fc48e7eb8c976a2920b7e40fa8fb2e2d8cf0
Colin Bruner <code@colinbruner.com>
2026-03-02 19:47:41 -0600

    add cloudflare warp connector role

---
+++ ansible/warp-connector/install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+TARGET=$1
+if [[ -z $TARGET ]]; then
+  echo "[ERROR]: Missing target. Rerun: $0 <target>"
+  exit 1
+fi
+
+# Read the WARP Connector token from 1Password.
+# NOTE: Create this item in 1Password before running.
+#       The token is obtained from Cloudflare Zero Trust > Networks > WARP Connector.
+WARP_TOKEN=$(op read "op://lab/cloudflare-warp-connector/token")
+
+# https://docs.ansible.com/ansible/latest/inventory_guide/intro_patterns.html#patterns-and-ansible-playbook-flags
+# NOTE: must have trailing comma
+ansible-playbook \
+    -u root \
+    -i "${TARGET}," \
+    --extra-vars "warp_connector_token=${WARP_TOKEN}" \
+    warp-connector.yml
